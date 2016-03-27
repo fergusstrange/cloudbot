e9c6deee32ed3b33bcdcaf484b714ea573856a36
Fergus Strange <fergus.strange@infinityworks.com>
2016-03-27 19:27:59 +0100

    Try out project reactor.

--- src/main/java/io/cloudbot/slack/DirectMessagePostedService.java
+++ src/main/java/io/cloudbot/slack/DirectMessagePostedService.java
@@ -12,6 +12,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import static reactor.core.publisher.Mono.fromCallable;
+
 @Component
 public class DirectMessagePostedService implements SlackMessagePostedListener {
 
@@ -29,17 +31,20 @@ public class DirectMessagePostedService implements SlackMessagePostedListener {
 
     @Override
     public void onEvent(SlackMessagePosted event, SlackSession session) {
-        logger.debug(formattedLogMessage(event));
-
-        if(slackAuthenticationService.userAuthenticated(event.getSender())) {
-            EC2InstanceCreationResult creationResult = ec2InstanceCreationService.createInstance(event);
-            slackMessageReply(event, session, "Instance creation in progress. Access your private key here...");
-            slackMessageReply(event, session, creationResult.getKeyRetrievalUrl());
-            slackMessageReply(event, session, String.format("Your new instance(s) are at %s", creationResult.getPublicIPs()));
-            slackMessageReply(event, session, "This link will expire in 5 minutes and cannot be recovered.");
-        } else {
-            slackMessageReply(event, session, "You're not allowed to do that...");
-        }
+        fromCallable(() -> {
+            logger.debug(formattedLogMessage(event));
+            return slackAuthenticationService.userAuthenticated(event.getSender());
+        }).consume(authenticated -> {
+            if (authenticated) {
+                EC2InstanceCreationResult creationResult = ec2InstanceCreationService.createInstance(event);
+                slackMessageReply(event, session, "Instance creation in progress. Access your private key here...");
+                slackMessageReply(event, session, creationResult.getKeyRetrievalUrl());
+                slackMessageReply(event, session, String.format("Your new instance(s) are at %s", creationResult.getPublicIPs()));
+                slackMessageReply(event, session, "This link will expire in 5 minutes and cannot be recovered.");
+            } else {
+                slackMessageReply(event, session, "You're not allowed to do that...");
+            }
+        });
     }
 
     private static SlackMessageHandle<SlackMessageReply> slackMessageReply(SlackMessagePosted event, SlackSession session, String message) {
